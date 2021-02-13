14d39e65bf578ac86f8389bae5df8f3f0073ce8e
BaquarAbbas <70859858+BaquarAbbas@users.noreply.github.com>
2021-02-13 22:52:08 +0530

    Update Day18:132 GeekyBuildings.py

--- Day18:132 GeekyBuildings.py
+++ Day18:132 GeekyBuildings.py
@@ -1,4 +1,12 @@
-''''''
+'''There are N buildings in Linear Land.
+They appear in a linear line one after the other and their heights are given in the array arr[].
+Geek wants to select three buildings in Linear Land and remodel them as recreational spots.
+The third of the selected building must be taller than the first and shorter than the second.
+Can geek build the three-building recreational zone?
+Example 1:
+  Input: N = 6, arr[] = {4, 7, 11, 5, 13, 2}
+  Output: True
+  Explanation: [4, 7, 5] fits the condition.'''
 
 #Solution 
 class Solution:
